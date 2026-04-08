b9b7e69e1d1183d80d2e2fcf24c8d7463e94292a
Angel Minga <afmingamedina@gmail.com>
2026-04-08 15:12:45 -0500

    chore: Update dependencies and SDK constraints
    - Updated various dependencies (e.g., `flutter_secure_storage`, `permission_handler`, `webview_flutter_android`) to their latest versions.
    - Adjusted Dart (`>=3.9.0 <4.0.0`) and Flutter (`>=3.35.0`) SDK constraints for compatibility.
    - Replaced `flutter_secure_storage_macos` with `flutter_secure_storage_darwin` in MacOS plugin registrations.

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,7 @@ import Foundation
 import audioplayers_darwin
 import file_picker
 import flutter_image_compress_macos
-import flutter_secure_storage_macos
+import flutter_secure_storage_darwin
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
@@ -21,7 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
